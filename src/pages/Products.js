7d9dc270f4ddacf241e84ee3947fe31a71ffbe55
snepraj2709 <snehaprajapatijnv15@gmail.com>
2023-08-12 23:00:45 +0530

    bug: useeffect dependency to update filterProduct

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -33,13 +33,14 @@ function Products() {
     const lowStockFiltered = lowStocks
       ? sortedArray?.filter((product) => product.stock < 10)
       : sortedArray;
+    filteredArray.push(...lowStockFiltered);
 
-    setFilteredProduct(lowStockFiltered);
+    setFilteredProduct(filteredArray);
   };
 
   useEffect(() => {
     filteredProducts();
-  }, []);
+  }, [state.selectedCategory, state.showLowStock, state.sortBy]);
 
   return (
     <div className="min-h-screen flex flex-col">
@@ -56,42 +57,46 @@ function Products() {
               <p className="col-span-1 text-lg">Stock</p>
               <p className="col-span-2 text-lg">Supplier</p>
             </div>
-            {filteredProduct?.map(
-              ({
-                id,
-                department,
-                name,
-                description,
-                price,
-                stock,
-                sku,
-                supplier,
-                delivered,
-                imageUrl,
-              }) => (
-                <div
-                  key={id}
-                  className=" flex flex-col w-full h-20 text-center"
-                  onClick={() => navigate(`/products/${id}`)}>
-                  <div className="grid grid-cols-12 gap-1 px-2 my-1">
-                    <img
-                      className="col-span-1 p-2 rounded-md object-cover w-16 h-16"
-                      src={imageUrl}
-                      alt={name}
-                    />
-                    <p className="col-span-3 text-lg line-clamp-1">{name}</p>
-                    <p className="col-span-4 text-lg line-clamp-1">
-                      {description}
-                    </p>
-                    <p className="col-span-1 text-base">{price}</p>
-                    <p className="col-span-1 text-base">{stock}</p>
-                    <p className="col-span-2 text-base line-clamp-1">
-                      {supplier}
-                    </p>
+            {filteredProduct.length > 0 ? (
+              filteredProduct?.map(
+                ({
+                  id,
+                  department,
+                  name,
+                  description,
+                  price,
+                  stock,
+                  sku,
+                  supplier,
+                  delivered,
+                  imageUrl,
+                }) => (
+                  <div
+                    key={id}
+                    className=" flex flex-col w-full h-20 text-center"
+                    onClick={() => navigate(`/products/${id}`)}>
+                    <div className="grid grid-cols-12 gap-1 px-2 my-1">
+                      <img
+                        className="col-span-1 p-2 rounded-md object-cover w-16 h-16"
+                        src={imageUrl}
+                        alt={name}
+                      />
+                      <p className="col-span-3 text-lg line-clamp-1">{name}</p>
+                      <p className="col-span-4 text-lg line-clamp-1">
+                        {description}
+                      </p>
+                      <p className="col-span-1 text-base">{price}</p>
+                      <p className="col-span-1 text-base">{stock}</p>
+                      <p className="col-span-2 text-base line-clamp-1">
+                        {supplier}
+                      </p>
+                    </div>
+                    <hr className="border border-slate-600" />
                   </div>
-                  <hr className="border border-slate-600" />
-                </div>
+                )
               )
+            ) : (
+              <div className="mt-5">Ooops! No products</div>
             )}
           </div>
         </div>
